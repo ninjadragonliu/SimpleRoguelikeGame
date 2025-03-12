947c3774bbd0c71bc7feaf83c3cc722d70880f12
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2025-03-11 21:04:12 -0400

    update

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,14 +1,21 @@
 extends CharacterBody2D
 
+func _physics_process(delta):
+	player_input()
+	
 func player_input() -> void:
 	if Input.is_action_just_pressed("move_right"):
 		velocity = Vector2.RIGHT
+		move(velocity)
 	elif Input.is_action_just_pressed("move_up"):
 		velocity = Vector2.UP
+		move(velocity)
 	elif Input.is_action_just_pressed("move_left"):
 		velocity = Vector2.LEFT
+		move(velocity)
 	elif Input.is_action_just_pressed("move_down"):
 		velocity = Vector2.DOWN
+		move(velocity)
 
 func move(direction: Vector2) -> void:
 	var space_rid = get_world_2d().space
@@ -17,5 +24,7 @@ func move(direction: Vector2) -> void:
 	var result = space_state.intersect_ray(query)
 	
 	if result:
-	if result.collider.is_in_group("Wall"):
-		return
+		if result.collider.is_in_group("Wall"):
+			return
+			
+	position += 48 * direction
