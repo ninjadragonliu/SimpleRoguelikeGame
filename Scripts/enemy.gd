74b54f11384707e0901899931fb72ac5d92ebff7
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2025-03-13 12:36:50 -0400

    added movement functionality to enemy

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,1 +1,35 @@
 extends CharacterBody2D
+
+@onready var player = get_tree().get_nodes_in_group("Player")[0]
+
+var health : int = 3
+var damage : int = 1
+var attack_chance : float = 0.5
+
+func move() -> void:
+	if randf() < 0.5:
+		return
+	var direction : Vector2 = Vector2.ZERO
+	var can_move : bool = false
+	while(can_move == false):
+		direction = get_random_direction()
+		var space_rid = get_world_2d().space
+		var space_state = PhysicsServer2D.space_get_direct_state(space_rid)
+		var query = PhysicsRayQueryParameters2D.create(global_position, global_position + Vector2(48, 48) * direction)
+		var result = space_state.intersect_ray(query)
+		if not result and position + 48 * direction != player.position:
+			can_move = true
+	position += 48 * direction
+
+func get_random_direction() -> Vector2:
+	var ran : int = randi_range(0, 3)
+	match ran:
+		0:
+			return Vector2.UP
+		1:
+			return Vector2.DOWN
+		2:
+			return Vector2.RIGHT
+		3:
+			return Vector2.LEFT
+	return Vector2.ZERO
