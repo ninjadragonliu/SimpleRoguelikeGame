c544df307e4d0108e07379d20a99476c1972baac
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2025-03-13 13:10:52 -0400

    updated enemy script

--- Scripts/enemies.gd
+++ Scripts/enemies.gd
@@ -5,6 +5,13 @@ var player : CharacterBody2D
 
 func _ready():
 	player = get_tree().get_nodes_in_group("Player")[0]
+	player.connect("player_moved", Callable(self, "on_player_moved"))
+	check_enemies()
 
 func check_enemies() -> void:
 	enemies = get_tree().get_nodes_in_group("Enemy")
+
+func on_player_moved() -> void:
+	for enemy in enemies:
+		if enemy:
+			enemy.move()
