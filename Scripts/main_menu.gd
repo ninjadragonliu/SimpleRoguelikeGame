9c9904bc5afb0d8a505f527672a32594364acccc
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2025-03-14 21:40:27 -0400

    created main menu

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,29 @@
+extends Control
+
+
+
+
+func _on_play_pressed() -> void:
+	$Intro.visible = false
+	$Play.visible = true
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_start_pressed() -> void:
+	if $Play/Buttons/SpinBox.value == 0:
+		Global.seed = randi_range(1, 100000)
+	else:
+		Global.seed = $Play/Buttons/SpinBox.value
+	
+	Global.health = Global.max_health
+	Global.level = 1
+	Global.coins = 0
+	get_tree().change_scene_to_file("res://Scenes/world.tscn")
+
+
+func _on_back_pressed() -> void:
+	$Intro.visible = true
+	$Play.visible = false
